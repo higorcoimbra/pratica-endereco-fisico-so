391a4f88e05d074cce9b4ba557f23879e55e169d
Higor Coimbra <higor1308@gmail.com>
2017-05-24 09:06:43 -0300

    modificando comentario das hipoteses

---
+++ teste.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	unsigned long a;
+	unsigned long b;	
+
+	a = 10;
+	b = 3;
+
+	printf("Divisão igual a %lu\n",a/b);	
+	printf("Tamanho do inteiro sem sinal %ld\n",sizeof(unsigned long));	
+
+
+	return 0;
+}
